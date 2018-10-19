3088e924c29f2e5039b7e1e8d2f4fae56e53510d
Harry Hummel <hhhummel@comcast.net>
2018-10-19 14:31:18 -0400

    Dummy react front end using fusionbox.com/blog/detail/create-react-app-and-django/624/

--- a/urls.py
+++ a/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ... the rest of the urlpatterns ...
+    # must be catch-all for pushState to work
+    url(r'^', views.FrontendAppView.as_view()),
 ]
+
+
+
+
+
+#from django.urls import path
+
+#from . import views
+
+#urlpatterns = [
+#    path('', views.index, name='index'),
+#]
