c0351ac7516e8f21143eebb36fe1205c525d9bb1
Harry Hummel <hhhummel@comcast.net>
2018-10-19 22:28:19 -0400

    Add frontend/build

---
+++ frontend/build/precache-manifest.d6270244555ef64144fedf6410af1ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a6bd7cf746b58a3ae0d",
+    "url": "/static/js/main.9a6bd7cf.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "9a6bd7cf746b58a3ae0d",
+    "url": "/static/css/main.6ed91e52.chunk.css"
+  },
+  {
+    "revision": "85f4354a9873e4029714fee304c8f5c8",
+    "url": "/index.html"
+  }
+];
